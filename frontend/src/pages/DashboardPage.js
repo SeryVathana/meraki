b81d2e38f2e704b356dd434f47c3b06f42047096
SeryVathana <yooseryvathana@gmail.com>
2024-06-09 20:58:28 +0700

    asdadasd

--- frontend/src/pages/DashboardPage.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Navigate } from "react-router-dom";
-const DashboardPage = () => {
-    return _jsx(Navigate, { to: "/dashboard/overview" });
-};
-export default DashboardPage;
